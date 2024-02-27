d877acfa0235927b348a8bfd864bf1af1e0285fe
Sirojiddin Aripov <aripov86cm@gmail.com>
2024-02-26 19:06:15 -0500

    Location permission is set up

--- app/src/main/java/com/aripov/qiblafindercompose/MainActivity.kt
+++ app/src/main/java/com/aripov/qiblafindercompose/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.aripov.qiblafindercompose
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.android.gms.location.LocationServices
+import androidx.core.content.ContextCompat
 import com.aripov.qiblafindercompose.ui.theme.QiblaFinderComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             QiblaFinderComposeTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,11 +45,77 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(name = "Sirojiddin")
+                    LocationScreen()
                 }
             }
         }
     }
+}
+@Composable
+fun LocationScreen() {
+    val context = LocalContext.current
+    var location by remember { mutableStateOf("Your location") }
+
+    //Create permission launcher
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted: Boolean ->
+            if (isGranted) {
+                //Permission granted, update the location
+                getCurrentLocation(context) { lat, long ->
+                    location = "Latitude: $lat, Longitude: $long"
+                }
+            }
+        })
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Button(
+            onClick = {
+                if(hasLocationPermission(context)) {
+                    //permission already granted, update the location
+                    getCurrentLocation(context) {lat, long ->
+                        location = "Latitude: $lat Longitude: $long"
+                    }
+                } else {
+                    //Request location permission
+                    requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+        ) {
+            Text(text = "Allow")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = location)
+    }
+}
+
+@SuppressLint("MissingPermission")
+fun getCurrentLocation(context: Context, callback: (Double, Double) -> Unit) {
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+    fusedLocationClient.lastLocation
+        .addOnSuccessListener { location ->
+            if(location != null) {
+                val lat = location.latitude
+                val long = location.longitude
+                callback(lat, long)
+            }
+        }
+        .addOnFailureListener { exception ->
+            exception.printStackTrace()
+        }
+}
+
+fun hasLocationPermission(context: Context): Boolean {
+    return ContextCompat.checkSelfPermission(
+        context,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
 }
 
 @Composable
@@ -36,7 +125,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
